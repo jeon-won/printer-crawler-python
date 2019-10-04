b59824cae623930d735622752c53b1a168903a46
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2019-10-04 16:47:55 +0900

    공통적으로 사용되는 함수와 크롤링 함수에서 사용하는 설정 값

--- pntcrawler/config.py
+++ pntcrawler/config.py
@@ -1,4 +1,4 @@
 chromedriver_path = "C:/Users/user/Documents/Python/pntcrawler/chromedriver.exe" # 크롬드라이버 절대경로
-save_folder = "crawldata" # 크롤링 결과를 저장할 폴더 이름
+save_folder = "crawldata" # 크롤링 결과를 저장할 폴더
 save_file_prefix = "프린터크롤링" # 엑셀 파일명에 붙을 접두어
 strftime_param = "%Y-%m-%d_%H-%M-%S" # strftime() 함수에 적용할 매개변수
