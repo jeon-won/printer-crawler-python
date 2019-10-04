a4766dd2696c07b711ec15df3a504ede2665e5b8
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2019-10-05 00:29:47 +0900

    크롤링할 프린터 정보(부서명, 모델명, ip)

--- crawling_target.py
+++ crawling_target.py
@@ -1,23 +1,29 @@
+# 신도 D417, D716, CM3091 일부 모델
 dept_sindoh_d417 = [
   { 'dept': 'A부서', 'model': 'Sindoh D417', 'ip': '192.168.0.1' },
 ]
 
+# 신도 CM3091 모델
 dept_sindoh_cm3091 = [
   { 'dept': 'B부서', 'model': 'Sindoh CM3091', 'ip': '192.168.0.2' },
 ]
 
+# 제록스 DCIVC2265, APVC2275, APVC3373, DCVC3374, DCVC3376, DCVIC3371, DPC5005D 모델
 dept_xerox_c2265 = [
   { 'dept': 'C부서', 'model': 'Xerox DCIVC2265', 'ip': '192.168.0.3' },
 ]
 
+# 제록스 DP3055 모델
 dept_xerox_dp3055 = [
   { 'dept': 'D부서', 'model': 'Xerox DP3055', 'ip': '192.168.0.4' },
 ]
 
+# OKI C843 모델
 dept_oki_c843 = [
   { 'dept': 'E부서', 'model': 'OKI C843DN', 'ip': '192.168.0.5' }, 
 ]
 
+# OKI ES5112 모델
 dept_oki_es5112 = [
   { 'dept': 'F부서', 'model': 'OKI ES5112', 'ip': '192.168.0.6' }, 
 ]
