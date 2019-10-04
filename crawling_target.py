cba5bca846dee9215a66c35542397b4f4745bdc7
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2019-10-04 16:33:14 +0900

    크롤링할 프린터 정보(부서명, 모델명, ip)

---
+++ crawling_target.py
@@ -0,0 +1,23 @@
+dept_sindoh_d417 = [
+  { 'dept': 'A부서', 'model': 'Sindoh D417', 'ip': '192.168.0.1' },
+]
+
+dept_sindoh_cm3091 = [
+  { 'dept': 'B부서', 'model': 'Sindoh CM3091', 'ip': '192.168.0.2' },
+]
+
+dept_xerox_c2265 = [
+  { 'dept': 'C부서', 'model': 'Xerox DCIVC2265', 'ip': '192.168.0.3' },
+]
+
+dept_xerox_dp3055 = [
+  { 'dept': 'D부서', 'model': 'Xerox DP3055', 'ip': '192.168.0.4' },
+]
+
+dept_oki_c843 = [
+  { 'dept': 'E부서', 'model': 'OKI C843DN', 'ip': '192.168.0.5' }, 
+]
+
+dept_oki_es5112 = [
+  { 'dept': 'F부서', 'model': 'OKI ES5112', 'ip': '192.168.0.6' }, 
+]
