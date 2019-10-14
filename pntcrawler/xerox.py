09fc42a93e26858c771d15994e9f238b883cf395
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2019-10-14 09:34:18 +0900

    제록스 프린터 크롤링 모듈

--- pntcrawler/xerox.py
+++ pntcrawler/xerox.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from pntcrawler.common import get_empty_data, get_page_source
 
+
 # 작동 모델(컬러복합기): Xerox DCIVC2265, APVC2275, APVC3373, DCVC3374, DCVC3376, DCVIC3371
 # 작동 모델(컬러프린터): Xerox DPC5005D
 def get_xerox_c2265(dept, model, ip):
@@ -32,6 +33,48 @@ def get_xerox_c2265(dept, model, ip):
         print("성공!")
         return info
 
+    except Exception as ex:
+        data = get_empty_data(ex, dept, model, ip)
+        return data
+
+
+# 작동 모델(컬러복합기): APVC5580, DCVC5585
+def get_xerox_c5580(dept, model, ip):
+    try: 
+        print(dept + " 부서의 " + model + " 소모품 정보 크롤링 중...")
+
+        # 크롤링
+        url = "http://" + ip + "/stsply.htm"
+        source = get_page_source(url)
+        soup = BeautifulSoup(source, 'html.parser')
+        data = soup.findAll('small')
+        
+        # 이 컬러복합기는 검정 토너를 2개 장착함
+        toner_k1 = int(data[5].contents[0].replace('%', ''))
+        toner_k2 = int(data[8].contents[0].replace('%', ''))
+
+        # Dictionary 생성
+        info = {
+            'dept': dept,
+            'model': model,
+            'ip': ip,
+            'toner_k': toner_k1 if toner_k1 < toner_k2 else toner_k2,  # 두 블랙 토너 중 작은 값
+            'toner_c': int(data[11].contents[0].replace('%', '')), 
+            'toner_m': int(data[14].contents[0].replace('%', '')), 
+            'toner_y': int(data[17].contents[0].replace('%', '')), 
+            'drum_k' : str(data[25].contents[0]), 
+            'drum_c' : str(data[27].contents[0]), 
+            'drum_m' : str(data[29].contents[0]), 
+            'drum_y' : str(data[31].contents[0]), 
+            # 'staple_crtridge': str(data[35].contents[0]),  # 스테이플 카트리지
+            # 'staple_crtridge_front': str(data[39].contents[0]),  # 제본용 스테이플 카트리지(앞쪽)
+            # 'staple_crtridge_inside': str(data[41].contents[0]),  # 제본용 스테이플 카트리지(안쪽)
+            'note': f"Black 토너(K1): {toner_k1}, Black 토너(K2): {toner_k2}"  # 두 블랙 토너 정보
+        }
+
+        print("성공!")
+        return info
+    
     except Exception as ex:
         data = get_empty_data(ex, dept, model, ip)
         return data
