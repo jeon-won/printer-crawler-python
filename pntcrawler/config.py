c47d4cd3f9e9f4cf1989d71936a0810acad72c69
Jeon Won <56036601+jeon-won@users.noreply.github.com>
2019-10-31 11:32:12 +0900

    20191031_설정추가

--- pntcrawler/config.py
+++ pntcrawler/config.py
@@ -1,4 +1,17 @@
+# 크롬 드라이버
 chromedriver_path = "C:/Users/user/Documents/Python/pntcrawler/chromedriver.exe"  # 크롬드라이버 절대경로
+
+# 엑셀 파일 저장 관련
 save_folder = "crawldata"  # 크롤링 결과를 저장할 폴더 이름
 save_file_prefix = "복합기크롤링"  # 엑셀 파일명에 붙을 접두어
 strftime_param = "%Y-%m-%d_%H-%M-%S"  # strftime() 함수에 적용할 매개변수
+
+# 토너 주의 및 경고 색상
+warning_color = "FFFFDF00"  # 살구색(?)
+alert_color = "FFFFFF00"  # 노란색
+
+# 토너 주의 및 경고 잔량
+toner_warning = 50
+toner_alert = 25
+drum_warning = 30
+drum_alert = 20
